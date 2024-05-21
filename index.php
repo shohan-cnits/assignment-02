05bc312fd85d4aaa7d10f2fc2cb803a9a60b9899
shohan-cnits <shohan.cnits@gmail.com>
2024-05-21 12:45:42 +0600

    asseiment-02 done

---
+++ index.php
@@ -0,0 +1,45 @@
+
+<?php 
+
+
+$sum = 0;
+
+for ( $i = 1 ; $i <=30; $i++)
+{
+    $sum = $sum+$i ;
+};
+
+echo $sum;
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+for ($x=1 ; $x<=100; $x+=2){
+    echo  $x;
+}
+
+echo "<br>";
+echo "<br>";
+
+for ($x=2 ; $x<=100; $x+=2){
+    echo  $x;
+}
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$number = 5;
+
+for($y=1; $y<=10 ; $y++){
+   echo " $number x $y = " . $number * $y ."<br>";
+};
+
+ ?>
+
+
+
+
